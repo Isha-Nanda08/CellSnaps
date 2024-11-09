394f3106bd8aefd26533875d5518e223713284ba
Sunandhit-Gupta <sunandhitgupta@gmail.com>
2024-11-09 19:28:20 +0530

    header changes

--- cellsnaps/src/components/Header.js
+++ cellsnaps/src/components/Header.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../Styles/Header.css';
+import axios from '../api/axios';
 
 function Header() {
+
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+        // Call the backend logout endpoint
+        await axios.post('/auth/logout', {}, { withCredentials: true });
+        // Clear any client-side auth state here, if applicable
+        // e.g., setAuth(null) or useAuth().setAuth(null) if using context
+        // Redirect to the login page
+        navigate('/login');
+    } catch (error) {
+        console.error('Error logging out:', error);
+    }
+};
+
+
+
   return (
     <header className="header">
       <div className="logo">
@@ -15,7 +35,11 @@ function Header() {
         <a href="#analysis">Analysis & Modeling</a>
         <a href="#publications">Publications</a>
         <a href="#education">Education</a>
-        <a href="#support">Login</a>
+        <div className='wrapper_btn'>
+        <button onClick={handleLogout} style={{ padding: '8px 16px', cursor: 'pointer' }} id='login_btn' >
+            Logout
+        </button>
+        </div>
       </nav>
       <div className="search-icon">
         🔍
