c2281d412571a86857ab3b7b6f12a6e64f8cdc5a
Gautam Sodhi <129111379+Iam-Sodhi@users.noreply.github.com>
2024-11-09 14:23:32 +0530

    #. Configured tailwind and shadcn ui

--- cellsnaps/src/Pages/Home.js
+++ cellsnaps/src/Pages/Home.js
@@ -1,17 +1,24 @@
 // Home.js
 import React from 'react';
 import '../Styles/Home.css';
+import { Input } from '../components/ui/input.tsx';
+import { Label } from '../components/ui/label.tsx';
 const Home = () => {
   return (
-    <div className='main'>
+    <div  className='main'>
 
-    <form action="">
+    <form action="" >
     <label for="myfile">Upload a file:</label>
     <br/>
     <input type="file" id="myfile" name="myfile" accept='image/*'/>
     <br/>
     <input type="submit" value="Submit"/>
     </form>
+
+    <div className="grid w-full max-w-sm items-center gap-1.5">
+      <Label htmlFor="File" className='text-blue-600'>File</Label>
+      <Input id="file" type="file" />
+    </div>
 
     </div>
   );
